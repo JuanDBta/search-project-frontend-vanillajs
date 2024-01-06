aea9646e9d7e92f0a516e2da330393d058608c57
JuanDBta <simplebetsas@gmail.com>
2024-01-06 13:07:10 -0500

    Improve syntaxis and readeablity

--- JS Files/app.js
+++ JS Files/app.js
@@ -1,49 +1,52 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.getElementById('searchInput');
   const searchResultsList = document.getElementById('searchResultsList');
   const uniqueSearches = new Set();
   let searchTimeout;
 
+  const clearSearchResults = () => {
+    searchResultsList.innerHTML = '';
+  };
 
-  searchInput.addEventListener('input', function (event) {
-      const inputValue = event.target.value.trim();
-      clearTimeout(searchTimeout);
+  const isValidSearch = (value) => {
+    const regex = /^(\S+\s+){1}\S+.{3,}$/;
+    return regex.test(value);
+  };
 
-      searchTimeout = setTimeout(function () {
+  const findLargestSearch = (searchSet) => {
+    const searchArray = Array.from(searchSet);
+    const largestSearch = searchArray.reduce((largest, current) => (current.length > largest.length ? current : largest), '');
+    return largestSearch;
+  };
+
+  const filterSubstrings = (searchSet, largestSearch) => {
+    const searchArray = Array.from(searchSet);
+    const filteredArray = searchArray.filter((search) => search === largestSearch
+    || !search.includes(largestSearch));
+    const filteredSet = new Set(filteredArray);
+    return filteredSet;
+  };
+
+  const appendSearchResult = (result) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('search-result-item');
+    listItem.textContent = result;
+    searchResultsList.appendChild(listItem);
+  };
+
+  searchInput.addEventListener('input', (event) => {
+    const inputValue = event.target.value.trim();
+    clearTimeout(searchTimeout);
+
+    searchTimeout = setTimeout(() => {
       clearSearchResults();
       if (isValidSearch(inputValue)) {
-        // Agrega la búsqueda al conjunto único
         uniqueSearches.add(inputValue);
         const largestSearch = findLargestSearch(uniqueSearches);
         const filteredSearches = filterSubstrings(uniqueSearches, largestSearch);
 
-        // Muestra la búsqueda completa en la lista de resultados
         filteredSearches.forEach(appendSearchResult);
-    }
-  }, 1500);
+      }
+    }, 1500);
   });
-
-  function isValidSearch(value) {
-    const regex = /^(\S+\s+){1}\S+.{3,}$/;
-    return regex.test(value);
-}
-
-function findLargestSearch(searchSet) {
-  return Array.from(searchSet).reduce((largest, current) => current.length > largest.length ? current : largest, '');
-}
-
-function filterSubstrings(searchSet, largestSearch) {
-  return new Set(Array.from(searchSet).filter(search => search === largestSearch || !search.includes(largestSearch)));
-}
-
-  function appendSearchResult(result) {
-      const listItem = document.createElement('li');
-      listItem.classList.add('search-result-item');
-      listItem.textContent = result;
-      searchResultsList.appendChild(listItem);
-  }
 });
-
-function clearSearchResults() {
-  searchResultsList.innerHTML = '';
-}

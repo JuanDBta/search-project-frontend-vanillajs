7dae433a83cbe2327c845ee458e39033f9cb2ac4
JuanDBta <simplebetsas@gmail.com>
2024-01-06 12:17:06 -0500

    Added filter with regex

--- JS Files/app.js
+++ JS Files/app.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   const searchInput = document.getElementById('searchInput');
   const searchResultsList = document.getElementById('searchResultsList');
-  
-  searchInput.addEventListener('input', function (event) {
-      const inputValue = event.target.value;
 
-      if (inputValue.trim() !== '') {
-          appendSearchResult(inputValue);
-      }
+  searchInput.addEventListener('input', function (event) {
+      const inputValue = event.target.value.trim();
+      
+      if (isValidSearch(inputValue)) {
+        appendSearchResult(inputValue);
+    }
   });
+
+  function isValidSearch(value) {
+    const regex = /^(\S+\s+){1}\S+.{3,}$/;
+    return regex.test(value);
+}
 
   function appendSearchResult(result) {
       const listItem = document.createElement('li');
+      listItem.classList.add('search-result-item');
       listItem.textContent = result;
       searchResultsList.appendChild(listItem);
   }

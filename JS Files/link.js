9e22c631cfb7d578fe8f0a0b50391f4ebdc98dac
JuanDBta <simplebetsas@gmail.com>
2024-01-06 02:06:34 -0500

    Added js file and fix eslint errors

---
+++ JS Files/link.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const statisticsTitle = document.getElementById('statistics-title');
+  statisticsTitle.addEventListener('click', () => {
+    window.open('https://www.google.com', '_blank');
+  });
+});

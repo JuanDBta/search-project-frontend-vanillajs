67fb39bfd563551429f39e922afbd39db8e1ecb2
JuanDBta <simplebetsas@gmail.com>
2024-01-06 23:11:30 -0500

    Added card-1 with total searches by user

--- JS Files/app.js
+++ JS Files/app.js
@@ -40,6 +40,33 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
   };
 
+  // Function to fetch count searches by user from the server
+  const countSearches = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:3000/count_searches');
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching user searches:', error);
+      throw error;
+    }
+  };
+
+  // Function to display counts of searches by user in the user interface
+  const displayCountSearches = async () => {
+    try {
+      const getCountsData = await countSearches();
+      const countsCard = document.getElementById('counts');
+      console.log(getCountsData);
+      countsCard.textContent = `You have done ${getCountsData.searches_count} searches!`;
+    } catch (error) {
+      console.error('Error displaying user searches count:', error);
+    }
+  };
+
   // Function to append a new search result to the server
   const appendSearchResult = async (result) => {
     const jsonData = {
@@ -61,6 +88,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const postData = await response.json();
         console.log('Search created successfully:', postData);
         displaySearches(getData);
+        displayCountSearches();
       } else {
         console.error(`Failed to create search. HTTP error! Status: ${response.status}`);
       }
@@ -84,4 +112,5 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // Fetch initial data and display searches when the page loads
   displaySearches(getData);
+  displayCountSearches();
 });
